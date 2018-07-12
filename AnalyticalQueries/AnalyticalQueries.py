8f50ff52e775eedfa6d20639f6da2a79cabae2cf
afshin sadeghi <sadeghi@cs.uni-bonn.de>
2018-07-12 12:57:54 +0200

    Update AnalyticalQueries.py

--- AnalyticalQueries/AnalyticalQueries.py
+++ AnalyticalQueries/AnalyticalQueries.py
@@ -2,22 +2,33 @@ from textblob import TextBlob
 from SPARQLWrapper import SPARQLWrapper2
 
 
-# Sentiment Classifier in textblob:
-# TextBlob uses a large Movie Review Dataset which is pre-classified as positive and negative.
-# TextBlob trains using the Naive Bayes classifier to determine positive and negative reviews.
-# the sentiment classifier calculates the polarity of tokens between -1.0 and 1.0
-# -1.0 is negative, 0.0 is neutral and 1.0 is positive
+# The Query and sentiment analysis of the extracted information about reviews and their relation
+# to article and reviewers.
+# This component connects to a local fuseki server that stores the content of Article_RDF.ttl
+# and executes SPARQL queries over it. Then it processes the result of the queries and applies sentiment analysis
+# on them using a Naive Bayes sentiment classifier.
 
-def get_sentiment(text):
-    # make TextBlob object of passed text
-    analysis = TextBlob(text.strip())
-    # assign sentiment
-    if analysis.sentiment.polarity > 0:
+# The Sentiment Classifier in textblob:
+# TextBlob trains using the Naive Bayes classifier to determine positive and negative reviews.
+# It is trained on a Movie Review Dataset which is labled as positive and negative.
+# The classifier calculates the polarity of tokens between -1.0 and 1.0 . If we regard x as the polarity, x < 0 is 
+# negative polarity , 0.0 is neutral and x > 0 is positive polarity of the review.
+
+
+def get_sentiment(polarity):
+    if polarity > 0:
         return 'positive'
-    elif analysis.sentiment.polarity == 0:
+    elif polarity == 0:
         return 'neutral'
     else:
         return 'negative'
+
+
+def get_sentiment_polarity(text):
+    # make TextBlob object of passed text
+    analysis = TextBlob(text.strip())
+    # assign sentiment
+    return analysis.sentiment.polarity
 
 
 # Query 1: What range of text in the Requirements section have received the most attention from the reviewers.
@@ -114,6 +125,7 @@ SELECT ?sectionName  ?commentInSectionURI ?commentURI ?commentContent WHERE {
             comments_per_section[sectionName, commentURI] = commentContent
     return comments_per_section
 
+
 def getCommentsPerPerSectionPerUser():
     sparql = SPARQLWrapper2("http://localhost:3030/article/sparql")  # address of local fuseki server
     sparql.setQuery("""
@@ -129,17 +141,58 @@ SELECT ?sectionName  ?commentInSectionURI ?commentURI ?commentContent ?commentfr
 ?commentfromUserURI  <http://schema.org/creator> ?userUri
 }
     """)
-    comments_per_section = {}  # find those comments that are related to a section
+    comments_per_section_per_user = {}  # find those comments that are related to a section
     for result in sparql.query().bindings:
         sectionName = result["sectionName"].value
         commentInSectionURI = result["commentInSectionURI"].value.rsplit("/", 1)[1]
         commentURI = result["commentURI"].value.rsplit("#", 1)[1]
         commentContent = result["commentContent"].value
-        commentfromUserURI = result["commentfromUserURI"].value
-        userUri = result["userUri"].value
-        if commentInSectionURI == commentURI:
-            comments_per_section[sectionName, commentURI] = commentContent
-    return comments_per_section
+        commentfromUserURI = result["commentfromUserURI"].value.rsplit("#", 1)
+        if len(commentfromUserURI) > 1: commentfromUserURI = commentfromUserURI[1]
+
+        reviewerNumber = result["userUri"].value.rsplit("#", 1)
+        if len(reviewerNumber) > 1: reviewerNumber = reviewerNumber[1]
+
+        if commentInSectionURI == commentURI and commentURI.rsplit("-", 1)[1] == commentfromUserURI:
+            comments_per_section_per_user[sectionName, commentURI, reviewerNumber] = commentContent
+
+    return comments_per_section_per_user
+
+
+# Sentiment Query 1: Does the second reviewer have a positive idea about the "Introduction" section?
+def run_sentiment_query1(section_class_, reviewer_number_):
+    sum_polarity = 0
+    comments_per_section_per_user = getCommentsPerPerSectionPerUser()
+
+    for (section_class__, commentId, reviewer_number__), value in comments_per_section_per_user.iteritems():
+        if section_class__ == section_class_ and reviewer_number__ == reviewer_number_:
+            sum_polarity = sum_polarity + get_sentiment_polarity(value)
+
+    print "Sentiment Query 1: Does the second reviewer have a positive idea about the " + section_class_ + " section?"
+    print "result:"
+    print get_sentiment(sum_polarity)
+    return sum_polarity > 0
+
+
+# Sentiment Query 2: What was the section of article that reviewers were most unhappy about?
+# (I.e., which section needs to be revised with the highest priority?)
+def run_sentiment_query2():
+    sum_polarity = {}
+    min_polarity = 0
+    most_negative_reviewed_section = ""
+    comments_per_section = getCommentsPerSection()
+    for (section_class__, commentId), value in comments_per_section.iteritems():
+        a = sum_polarity.get(section_class__, 0)
+        sum_polarity[section_class__] = a + get_sentiment_polarity(value)
+
+    for (section_class__), value in sum_polarity.iteritems():
+        if value < min_polarity:
+            most_negative_reviewed_section = section_class__
+            min_polarity = value
+    print "Sentiment Query 2: What was the section of article that reviewers were most unhappy about?"
+    print "result: that section of paper is "
+    print most_negative_reviewed_section
+    return most_negative_reviewed_section, min_polarity
 
 
 # Query 1: returns range of text in the Requirements section have received the most attention from the reviewers.
@@ -166,14 +219,19 @@ run_query3("communication")
 # Output on the sample file:
 # comment-674297328: I do not understand the usage of communication here.
 
-def run_query4():
-    commentsPerPerSectionPerUser= getCommentsPerPerSectionPerUser()
 
-def run_query5():
-    comments_per_section= getCommentsPerSection()
+# Sentiment Query 1: Does the second reviewer have a positive idea about the "Requirements" section?
+reviewer_number = "4"
+run_sentiment_query1("Requirements", reviewer_number)
 
+# Output on the sample file:
+# result:
+# positive
 
-a = getCommentsPerSection()
-print a
+# Sentiment Query 2: What was the section of article that reviewers were most unhappy about?
+# (I.e., which section needs to be revised with the highest priority?)
+run_sentiment_query2()
 
-
+# Output on the sample file:
+# result: that section of paper is
+# Introduction
