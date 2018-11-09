9a6bc1e024ae3fcc5d8cfa1f9e7fb2f9714ff8ac
saptarshi.bhattacharya <saptarshi2009@hotmail.com>
2018-11-09 17:06:00 +0100

    Updated Code for htmlrdfa-exporter

---
+++ SourceCode/htmlrdfa-exporter/index.js
@@ -0,0 +1,151 @@
+import {createSlug} from "../exporter/tools/file"
+import {findImages} from "../exporter/tools/html"
+import {ZipFileCreator} from "../exporter/tools/zip"
+import {htmlExportTemplate} from "./templates"
+import {addAlert} from "../common"
+import {katexRender} from "../katex"
+import {BaseHTMLRDFaExporter} from "./base"
+import download from "downloadjs"
+
+export class HTMLRDFaExporter extends BaseHTMLRDFaExporter {
+    constructor(doc, bibDB, imageDB, citationStyles, citationLocales) {
+        super()
+        this.doc = doc
+        this.citationStyles = citationStyles
+        this.citationLocales = citationLocales
+        this.bibDB = bibDB
+        this.imageDB = imageDB
+        this.exportOne()
+    }
+
+    exportOne() {
+        addAlert('info', this.doc.title + ': ' + gettext(
+                'HTML export has been initiated.'))
+
+        this.joinDocumentParts().then(() => this.exportTwo()
+    )
+
+
+    }
+
+    exportTwo() {
+
+        let styleSheets = [],
+            math = false
+
+        let title = this.doc.title
+
+        let contents = this.contents
+
+        let equations = contents.querySelectorAll('.equation')
+
+        let figureEquations = contents.querySelectorAll('.figure-equation')
+
+        let comments = []
+        for (let comment in this.doc.comments) {
+            comments.push(this.doc.comments[comment])
+        }
+
+        if (equations.length > 0 || figureEquations.length > 0) {
+            math = true
+            styleSheets.push({
+                filename: 'katex.min.css'
+            })
+        }
+
+        for (let i = 0; i < equations.length; i++) {
+            let node = equations[i]
+            let formula = node.getAttribute('data-equation')
+            katexRender(formula, node, {
+                throwOnError: false
+            })
+        }
+        for (let i = 0; i < figureEquations.length; i++) {
+            let node = figureEquations[i]
+            let formula = node.getAttribute('data-equation')
+            katexRender(formula, node, {
+                displayMode: true,
+                throwOnError: false
+            })
+        }
+
+        let includeZips = []
+
+        let httpOutputList = findImages(contents)
+
+        contents = this.addSectionsTag(contents)
+
+        contents = this.addFigureNumbers(contents)
+
+        contents = this.converTitleToRDFa(contents)
+
+        contents = this.convertAbstractToRDF(contents)
+
+        contents = this.converAuthorsToRDFa(contents)
+
+        contents = this.convertCommentsToRDFa(contents)
+
+        let sidetagList = []
+        contents = this.convertSideCommentsToRDFa(contents,this.doc.comments, sidetagList)
+
+        contents = this.adjustSections(contents,sidetagList)
+
+     	contents = this.addRefeneces(contents)
+
+     	contents = this.addRefeneceRDFa(contents)
+
+
+        contents = this.cleanUpAttributes(contents)
+        let contentsCode = this.replaceImgSrc(contents.innerHTML)
+        contentsCode = this.cleanUpTags(contentsCode)
+
+        let dom = htmlExportTemplate({
+            part: false,
+            title,
+            metadata: this.doc.metadata,
+            settings: this.doc.settings,
+            styleSheets,
+            contents: contentsCode
+        })
+
+        dom = dom.replace('<html>','<html xmlns="http://www.w3.org/1999/xhtml" xml:base="http://www.dc4plus.com/references/rdf_sem.html" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:foaf="http://xmlns.com/foaf/0.1/" >')
+
+        let outputList = [{
+            filename: 'document.html',
+            contents: dom
+        }]
+        if (comments.length > 0) {
+            for (let i = 0; i < comments.length; i++) {
+                let node = comments[i]
+                outputList.push(this.createComment(node))
+            }
+
+        }
+
+        for (let i = 0; i < styleSheets.length; i++) {
+            let styleSheet = styleSheets[i]
+            if (styleSheet.contents) {
+                outputList.push(styleSheet)
+            }
+        }
+
+        if (math) {
+            includeZips.push({
+                'directory': '',
+                'url': window.staticUrl + 'zip/katex-style.zip',
+            })
+        }
+
+        let zipper = new ZipFileCreator(
+            outputList,
+            httpOutputList,
+            includeZips
+        )
+
+        zipper.init().then(
+            blob => download(blob, createSlug(title) + '.html.zip',
+            'application/zip')
+    )
+    }
+
+}
---
+++ SourceCode/htmlrdfa-exporter/tags.js
@@ -0,0 +1,70 @@
+export const TAGS = [
+    {
+        tag: 'deo:Acknowledgements',
+        texts: ['acknowledgement', 'acknowledgements']
+    },
+    {
+        tag: 'deo:FutureWork',
+        texts: ['outlook', 'futurework', 'roadmap', 'plan']
+    },
+    {
+        tag: 'deo:Conclusion',
+        texts: ['conclusion', 'conclusions']
+    },
+    {
+        tag: 'deo:Results',
+        texts: ['results']
+    },
+    {
+        tag: 'deo:Discussion',
+        texts: ['analysis', 'discussion', 'discussions']
+    },
+    {
+        tag: 'deo:RelatedWork',
+        texts: ['relatedwork', 'literaturereview']
+    },
+    {
+        tag: 'deo:Evaluation',
+        texts: ['validation', 'evaluation', 'experiments', 'comparison', 'experimental']
+    },
+    {
+        tag: 'deo:Motivation',
+        texts: ['motivation', 'casestudy']
+    },
+    {
+        tag: 'deo:ProblemStatement',
+        texts: ['problem', 'approach', 'casedescription']
+    },
+    {
+        tag: 'deo:Abstract',
+        texts: ['abstract', 'summary']
+    },
+    {
+        tag: 'deo:Introduction',
+        texts: ['introduction']
+    },
+    {
+        tag: 'deo:Methods',
+        texts: ['approach', 'methodology', 'methods', 'proposedsolution', 'proposedapproach']
+    },
+    {
+        tag: 'ssn:System',
+        texts: ['framework', 'structure', 'system', 'architecture', 'implementation', 'implementing', 'schema']
+    },
+    {
+        tag: 'swrc:Keywords',
+        texts: ['keywords']
+    },
+    {
+        tag: 'deo:Background',
+        texts: ['background', 'concepts']
+    },
+    {
+        tag: 'deo:Model',
+        texts: ['modeling', 'model', 'representation', 'modelling']
+    },
+    {
+        tag: 'deo:Reference',
+        texts: ['references', 'reference']
+    }
+]
